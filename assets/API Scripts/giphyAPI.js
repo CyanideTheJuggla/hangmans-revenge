b4f8c23d26d7283795722fe3b885b7834a3d2a70
Katelyn Bunker <kerbunker@gmail.com>
2021-11-17 21:06:06 -0500

    temp img html and gif

--- assets/API Scripts/giphyAPI.js
+++ assets/API Scripts/giphyAPI.js
@@ -2,12 +2,22 @@
 /* https://developers.giphy.com/docs/api/   */
 
 const GIPHY_API = {
-    pathURL: 'https://api.giphy.com/v1/gifs/search?api_key=9Mj8VtVxS0fIYl8rye5eyBzrMDtkHEXO&q=food'
+    pathURL: 'https://api.giphy.com/v1/gifs/search'
 }
 
 
 var getGif = function(word) {
     var apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=9Mj8VtVxS0fIYl8rye5eyBzrMDtkHEXO&q=" + word;
 
-    fetch(apiUrl);
-}
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                var imgEl = document.querySelector("#gif-img");
+                imgEl.setAttribute("src", data.data[0].images.downsized.url);
+                console.log(data);
+            });
+        } else {
+            alert('Something went wrong!');
+        }
+    });
+};
