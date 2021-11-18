a21a06f17aa1cfa60d38e8381c005c52718cfaee
Katelyn Bunker <kerbunker@gmail.com>
2021-11-17 20:31:56 -0500

    fetch api functions

--- assets/API Scripts/DictionaryAPI.js
+++ assets/API Scripts/DictionaryAPI.js
@@ -4,3 +4,10 @@
 const DictionaryAPI = {
     pathURL: "https://api.datamuse.com/"
 }
+
+var getWordList = function(category) {
+    var apiUrl = "https://api.datamuse.com/words?rel_trg=" + category;
+
+    fetch(apiUrl);
+}
+
--- assets/API Scripts/giphyAPI.js
+++ assets/API Scripts/giphyAPI.js
@@ -2,5 +2,12 @@
 /* https://developers.giphy.com/docs/api/   */
 
 const GIPHY_API = {
-    pathURL: 'https://api.giphy.com/v1/gifs/'
+    pathURL: 'https://api.giphy.com/v1/gifs/search?api_key=9Mj8VtVxS0fIYl8rye5eyBzrMDtkHEXO&q=food'
+}
+
+
+var getGif = function(word) {
+    var apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=9Mj8VtVxS0fIYl8rye5eyBzrMDtkHEXO&q=" + word;
+
+    fetch(apiUrl);
 }
