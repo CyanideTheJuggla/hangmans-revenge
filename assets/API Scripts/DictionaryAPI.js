188856fd8d5b8178e952052acd7dbb8c7e1b2374
Katelyn Bunker <kerbunker@gmail.com>
2021-11-22 16:00:45 -0500

    Add random category and definition functionality

--- assets/API Scripts/DictionaryAPI.js
+++ assets/API Scripts/DictionaryAPI.js
@@ -1,9 +1,19 @@
 /* Using DataMuse API to get word lists */
 /* https://www.datamuse.com/api/        */
 
+// Array of categories for words
+var categoryArray = ['art', 'astronomy', 'animal', 'color', 'cooking', 'emotion', 'holiday', 'music', 'science', 'vehicle', 'weather'];
+var currentWord;
+
 const DictionaryAPI = {
     getRandomWord: getRandomWord
+};
+
+var randNum = function(max, min = 0) {
+    var num = Math.floor(Math.random() * (max - min) + min);
+    return num;
 }
+
 
 var getRandomWord = function(category) {
     var apiUrl = "https://api.datamuse.com/words?rel_trg=" + category + "&md=d";
@@ -12,15 +22,43 @@ var getRandomWord = function(category) {
         if (response.ok) {
             response.json().then(function(data) {
                 var maxLength = data.length;
-                var randNum = Math.floor(Math.random() * maxLength);
-                var randWord = (data[randNum]);
-                console.log(data[randNum]);
+                var randomNum = randNum(maxLength);
+                var randWord = (data[randomNum]);
+                console.log(randWord);
+                currentWord = randWord;
                 return randWord;
                 
             });
         } else {
-            alert('Something went wrong!');
+            var alertText = document.createElement("p");
+            alertText.textContent = "Word could not be retrieved";
         }
     });
+};
+
+var getRandomCategory = function() {
+    var randomNum = randNum(categoryArray.length);
+    var randCat = categoryArray[randomNum];
+    console.log(randCat);
+    var randomWord = getRandomWord(randCat);
+    return randomWord;
 }
+
+var getDefinition = function() {
+    console.log(currentWord);
+    var defDiv = document.createElement("div");
+    var defListEl = document.createElement("ul");
+    defDiv.appendChild(defListEl);
+    for ( var i = 0; i < currentWord.defs.length; i++) {
+        console.log(currentWord.defs[i]);
+        var def = document.createElement("p");
+        def.setAttribute("id", "def-" + i);
+        def.textContent = currentWord.defs[i];
+        defListEl.appendChild(def);
+    }
+    document.body.appendChild(defDiv);
+}
+
+
+
 
